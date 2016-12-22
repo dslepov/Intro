3250e2798480f4224a56226b810641e819f9b9b5
dslepov <dan.slepoff@yandex.ru>
2016-12-22 08:27:14 +0500

    На вход подается количество учеников в 3 классах. За каждой партой сидят двое. Вывести наименьшее число парт, которое нужно приобрести для 3 классов. Каждый класс сидит в своем кабинете.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main() {
-    int n, q, r;
-    cin >> n;
-    q = (n + 1) % 2;
-    r = n + q + 1;
-    cout << r;
+    int n1, n2, n3;
+    int p1, p2, p3, ps;
+    cin >> n1 >> n2 >> n3;
+    p1 = n1 % 2 + n1 / 2;
+    p2 = n2 % 2 + n2 / 2;
+    p3 = n3 % 2 + n3 / 2;
+    ps = p1 + p2 + p3;
+    cout << ps;
     return 0;
 }
